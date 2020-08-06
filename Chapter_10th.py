871d50a02604c2ab2be1405cf98b33ab68410463
Sana <Sana-Rasheed@users.noreply.github.com>
2020-08-06 12:43:39 +0300

    Add files via upload

--- Chapter_10th.py
+++ Chapter_10th.py
@@ -3,7 +3,7 @@
 f = open("demofile.txt", "r")
 print(f.read())
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_open.py 
 # Hello! Welcome to demofile.txt
 # This file is for testing purposes.
@@ -13,7 +13,7 @@ print(f.read())
 f = open("demofile.txt", "r")
 print(f.read(5))
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_open2.py
 # Hello
 
@@ -21,63 +21,63 @@ print(f.read(5))
 f = open("demofile.txt", "r")
 print(f.readline())
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_readline.py
 # Hello! Welcome to demofile.txt
 
 
-#Example 4
+# Example 4
 f = open("demofile.txt", "r")
 print(f.readline())
 print(f.readline())
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_readline2.py
 # Hello! Welcome to demofile.txt
 # This file is for testing purposes.
 
 
-# Example 4
+# Example 5
 f = open("demofile.txt", "r")
 for x in f:
   print(x)
 
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_readline3.py
 # Hello! Welcome to demofile.txt
 # This file is for testing purposes.
 # Good Luck!
 
-# Example 5
+# Example 6
 
 f = open("demofile.txt", "r")
 print(f.readline())
 f.close()
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_close.py
 # Hello! Welcome to demofile.txt
 
 
-# Example 6
+# Example 7
 f = open("demofile2.txt", "a")
 f.write("Now the file has more content!")
 f.close()
 
-#open and read the file after the appending:
+# open and read the file after the appending:
 f = open("demofile2.txt", "r")
 print(f.read())                    
 
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_append.py
 # Hello! Welcome to demofile2.txt
 # This file is for testing purposes.
 # Good Luck!Now the file has more content!
 
 
-# Example 7
+# Example 8
 f = open("demofile3.txt", "w")
 f.write(" I have deleted the content!")
 f.close()
@@ -87,16 +87,16 @@ f = open("demofile3.txt", "r")
 print(f.read())                
 
 
-# OUT
+# OUTPUT
 # C:\Users\My Name>python demo_file_write.py
 #  I have deleted the content!
 
 
-# Example 8
+# Example 9
 import os    
 os.remove("demofile.txt")
 
-# Example 9
+# Example 10
 import os
 if os.path.exists("demofile.txt"):
   os.remove("demofile.txt")
@@ -104,7 +104,7 @@ else:
   print("The file does not exist")
 
 
-# Example 10 
+# Example 11 
 # Create myfolder in working directory before executing code below.
 import os
 os.rmdir("myfolder")
